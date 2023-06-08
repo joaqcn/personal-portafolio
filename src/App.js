7fa8b2ce80728baab258264b76a6d97ad9b2f837
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-06-08 00:06:50 -0400

    Added scroll animation // does not work right now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect } from "react";
 import AboutMe from "./pages/aboutme";
 import Skills from "./pages/skills";
 import Projects from "./pages/projects";
-import backgroundPic from "../src/assets/background.jpg";
 
 function App() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll(".page-section");
+
+      sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        // Check if the section is in the viewport
+        if (rect.top <= windowHeight * 0.75 && rect.bottom >= 0) {
+          section.classList.add("animate-fadeIn");
+        } else {
+          section.classList.remove("animate-fadeIn");
+        }
+      });
+    };
+
+    // Initial check when the page loads
+    handleScroll();
+
+    // Check sections on scroll
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="relative h-screen">
-      <AboutMe />
-      <Skills />
-      <Projects />
-      <div className="absolute inset-0 bg-cover bg-center z-0" style={{ backgroundImage: `url(${backgroundPic})`, opacity:0.4 }}></div>
+    <div>
+      <div className="page-section animate-fadeIn">
+        <AboutMe />
+      </div>
+      <div className="page-section animate-fadeIn">
+        <Skills />
+      </div>
+      <div className="page-section animate-fadeIn">
+        <Projects />
+      </div>
     </div>
   );
 }
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -18,46 +18,46 @@ import {
 const Skills = () => {
   let size = "5em";
   return (
-    
-    <div className=" text-center flex flex-wrap justify-center items-center">
-      <div className="m-4">
-        <DiReact color="#61DBFB" size={size} />
-        <p>React</p>
+    <div className="container mx-auto py-12">
+      <h2 className="text-3xl font-semibold mb-8 text-center">Skills</h2>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        <div className="flex flex-col items-center hover:scale-105 transform transition-transform duration-300">
+          <DiReact color="#61DBFB" size={size} />
+          <p className="mt-2">React</p>
+        </div>
+        <div className="flex flex-col items-center hover:scale-105 transform transition-transform duration-300">
+          <DiJavascript1 color="#F7DF1E" size={size} />
+          <p className="mt-2">JavaScript</p>
+        </div>
+        <div className="flex flex-col items-center hover:scale-105 transform transition-transform duration-300">
+          <DiNodejsSmall color="#68A063" size={size} />
+          <p className="mt-2">Node.js</p>
+        </div>
+        <div className="flex flex-col items-center hover:scale-105 transform transition-transform duration-300">
+          <DiMongodb color="#47A248" size={size} />
+          <p className="mt-2">MongoDB</p>
+        </div>
+        <div className="flex flex-col items-center hover:scale-105 transform transition-transform duration-300">
+          <DiPython color="#3572A5" size={size} />
+          <p className="mt-2">Python</p>
+        </div>
+        <div className="flex flex-col items-center hover:scale-105 transform transition-transform duration-300">
+          <DiJava color="#007396" size={size} />
+          <p className="mt-2">Java</p>
+        </div>
+        <div className="flex flex-col items-center hover:scale-105 transform transition-transform duration-300">
+          <DiGit color="#F05032" size={size} />
+          <p className="mt-2">Git</p>
+        </div>
+        <div className="flex flex-col items-center hover:scale-105 transform transition-transform duration-300">
+          <DiGithubBadge color="#181717" size={size} />
+          <p className="mt-2">GitHub</p>
+        </div>
+        <div className="flex flex-col items-center hover:scale-105 transform transition-transform duration-300">
+          <DiUnitySmall color="#000000" size={size} />
+          <p className="mt-2">Unity</p>
+        </div>
       </div>
-      <div className="m-4">
-        <DiJavascript1 color="#F7DF1E" size={size} />
-        <p>JavaScript</p>
-      </div>
-      <div className="m-4">
-        <DiNodejsSmall color="#68A063" size={size} />
-        <p>Node.js</p>
-      </div>
-      <div className="m-4">
-        <DiMongodb color="#47A248" size={size} />
-        <p>MongoDB</p>
-      </div>
-      <div className="m-4">
-        <DiPython color="#3572A5" size={size} />
-        <p>Python</p>
-      </div>
-      <div className="m-4">
-        <DiJava color="#007396" size={size} />
-        <p>Java</p>
-      </div>
-      <div className="m-4">
-        <DiGit color="#F05032" size={size} />
-        <p>Git</p>
-      </div>
-      <div className="m-4">
-        <DiGithubBadge color="#181717" size={size} />
-        <p>GitHub</p>
-      </div>
-      <div className="m-4">
-        <DiUnitySmall color="#000000" size={size} />
-        <p>Unity</p>
-      </div>
-
-      
     </div>
   );
 };
